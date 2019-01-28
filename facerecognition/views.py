27df95a7c8a567c385d2d237d36f2dc5c56f8baf
ajitesh-30 <ajiteshs10@gmail.com>
2019-01-28 21:40:07 +0530

    Adding the function for creating dataset for a single user

---
+++ facerecognition/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+import cv2
+import numpy as np 
+from .settings import BASE_DIR
+def index(request):
+	return render(request,'index.html')
+
+def create_dataset(request):
+
+	enrollment = request.POST['enrollment']
+	detect_face = cv2.CascadeClassifier(BASE+DIR+'/util/haarcascade_frontalface_default.xml')
+	camera  = cv2.VideoCapture(0)
+	
+	image_id = enrollment
+
+	sampleNum = 0
+
+	while True:
+
+		ret,img = camera.read()
+
+		gray = 	cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+		faces = faceDetect.detectMultiScale(gray,1.3,5)
+
+		for x,y,w,h in faces:
+			
+			samepleNum = sampleNum+1
+
+			cv2.imwrite(BASE_DIR+'/util/dataset/user.'+str(enrollment)+'.'+str(sampleNum)+'.jpg',gray[y:y+h,x:x+w])
+
+			cv2.rectangle(img,x,y,(x+w,y+h),(0,255,0),2)
+
+			cv2.waitKey(250)
+
+		cv2.imshow("Face",img)
+		cv2.waitKey(1)
+
+		if samepleNum>35:
+			break
+	camera.release()
+
+	cv2.destroyAllWindows()
+
+	return redirect('/')
+
+
+
+
+
+
+
